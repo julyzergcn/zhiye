f0f3ac9f19d0b5a7ac81ba2efdc9d2dfba057233
Long <julyzergcn1@gmail.com>
2013-02-17 15:09:32 +0800

    bookmark link clickable

--- apps/bookmark/admin.py
+++ apps/bookmark/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from bookmark.models import Bookmark, Tag
 
 class BookmarkAdmin(admin.ModelAdmin):
-    exclude = ('activate_date', 'deactivate_date')
-    list_display = ('link', 'title', 'status')
-    list_filter = ('status', 'tags', 'owner')
+    exclude = ('activate_date', 'deactivate_date', 'owner')
+    list_display = ('title', 'link_url', 'tags_list', 'status')
+    list_filter = ('status', 'tags')
+    list_editable = ('status', )
+    
+    def link_url(self, obj):
+        return '<a href="%s">%s</a>' % (obj.link, obj.link)
+    link_url.allow_tags = True
+    
+    def tags_list(self, obj):
+        return ', '.join([t.title for t in obj.tags.all()])
     
     def save_model(self, request, obj, form, change):
+        obj.title = obj.title or 'nt'
         obj.owner = request.user
         obj.save()
     
